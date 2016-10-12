f8c0f252b25408480f45f788cfc7fe4cb329432a
gerry ens <gens@balihoo.com>
2016-10-12 16:23:17 -0600

    master: setup.py fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='blambda',
       version='0.1.0',
       description='Balihoo Command Line Tools for AWS Lambda function management',
-      install_requires=['boto3']
+      install_requires=['boto3'],
       packages=['blambda'],
       entry_points={
           'console_scripts': [
