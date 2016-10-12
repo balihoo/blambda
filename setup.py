9365833186573776d57559ee097c7c27c9313f59
gerry ens <gens@balihoo.com>
2016-10-12 16:29:43 -0600

    include all but tests

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='blambda',
       version='0.1.0',
       description='Balihoo Command Line Tools for AWS Lambda function management',
       install_requires=['boto3'],
-      packages=['blambda'],
+      packages=find_packages(exclude=['tests']),
       entry_points={
           'console_scripts': [
               'blambda = blambda.__main__:main',
