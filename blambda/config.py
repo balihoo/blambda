6d1abf67b22c18617b58a18dd893d71650a20f49
gerry ens <gens@balihoo.com>
2016-11-03 10:36:20 -0600

    installfix: local config

--- blambda/config.py
+++ blambda/config.py
@@ -3,28 +3,49 @@ import json
 import argparse
 
 configfile=os.path.abspath(os.path.join(os.path.expanduser('~'), '.config', 'blambda', 'config.json'))
+configfile_local=os.path.abspath(os.path.join(os.get_cwd(), '.blambda', 'config.json'))
 
-def save(var, val):
+def save(var, val, local):
     cfg = load()
-    cfg[var] = val
-    cfgdir = os.path.dirname(configfile)
+    if val is None:
+        #remove if setting existing value to None
+        if var in cfg:
+            del cfg[var]
+    else:
+        cfg[var] = val
+    cfgfile = configfile_local if local else configfile
+    cfgdir = os.path.dirname(cfgfile)
     if not os.path.exists(cfgdir):
         os.makedirs(cfgdir)
-    with open(configfile, 'w') as f:
+    with open(cfgfile, 'w') as f:
         f.write(json.dumps(cfg, sort_keys=True, indent=2))
 
 def load():
+    config = {}
+    # load global config
     if os.path.exists(configfile):
         with open(configfile) as f:
-            return json.load(f)
-    return {}
+            config = json.load(f)
+    # override with local config
+    if os.path.exists(configfile_local):
+        with open(configfile_local) as f:
+            config.update(json.load(f))
+     return config
 
 def main(args=None):
     parser = argparse.ArgumentParser("configure blambda")
+    parser.add_argument('action', choices=['set_local', 'set_global', 'get'])
     parser.add_argument('variable', choices=['region', 'environment', 'role', 'application'])
-    parser.add_argument('value', type=str, help='the value to give to the variable')
+    parser.add_argument('value', type=str, help='the value to give to the variable', default=None)
     args = parser.parse_args(args)
-    save(args.variable, args.value)
+    if args.action == 'set_local':
+        save(args.variable, args.value, local=True)
+    elif args.action == 'set_global':
+        save(args.variable, args.value, local=False)
+    else:
+        for k, v in sorted(load().items()):
+            if args.value is None or k == args.value:
+                print("{}: {}".format(k, v))
 
 if __name__ == '__main__':
     main()
