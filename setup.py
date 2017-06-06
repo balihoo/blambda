665e5cc47a61f89b95cd8940a233f0280ec08ecb
Bijan <bhoule@balihoo.com>
2017-06-06 14:14:22 -0600

    MKTR-13843: remove futures backport lib (not needed in python3.6)

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(name='blambda',
-      version='0.2.0',
+      version='0.2.1',
       description='Balihoo Command Line Tools for AWS Lambda function management',
       author='Gerry Ens',
       author_email='gens@balihoo.com',
       license='MIT',
       url='git@github.com:balihoo/fulfillment-lambda-functions.git',
-      install_requires=['boto3', 'python-dateutil', 'futures', 'requests', 'termcolor'],
+      install_requires=['boto3', 'python-dateutil', 'requests', 'termcolor'],
       packages=find_packages(exclude=['tests']),
       include_package_data=True,
       entry_points={
