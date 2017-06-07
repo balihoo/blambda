bdfcc2382a490b3e8024cebf7b735cae4e974e71
Bijan <bhoule@balihoo.com>
2017-06-07 14:27:39 -0600

    fix issue where namespaced packages weren't found

--- blambda/setup_libs.py
+++ blambda/setup_libs.py
@@ -7,6 +7,8 @@ import subprocess as sp
 import os
 
 from termcolor import cprint
+
+from .utils.findfunc import find_manifest
 from .utils import env_manager
 from .utils.lambda_manifest import LambdaManifest
 
@@ -14,20 +16,6 @@ from .utils.lambda_manifest import LambdaManifest
 def read_function_names_from_file(filename):
     with open(filename) as f:
         return {line.strip() for line in f}
-
-
-def default_lambda_functions_search_dir():
-    try:
-        return sp.check_output('git rev-parse --show-toplevel', shell=True, universal_newlines=True).strip()
-    except sp.CalledProcessError:
-        return '.'
-
-
-def find_function_manifest(func_name, search_root=default_lambda_functions_search_dir()):
-    for (root, dirs, files) in os.walk(search_root):
-        for file in files:
-            if file == func_name + '.json':
-                return os.path.join(root, file)
 
 
 def main(args=None):
@@ -45,7 +33,7 @@ def main(args=None):
         cprint("read {} from {}".format(func_names, args.file), 'blue')
 
     for func_name in func_names:
-        manifest_filename = find_function_manifest(func_name)
+        manifest_filename = find_manifest(func_name)
         if not manifest_filename:
             cprint("unable to find " + func_name, 'red')
             exit(1)
