14447e58135237ac3a7b5e04c9bbd44b88cd6fed
Bijan <bhoule@balihoo.com>
2017-09-08 16:12:47 -0600

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='blambda',
-      version='0.3.10',
+      version='0.3.11',
       description='Balihoo Command Line Tools for AWS Lambda function management',
       author='Balihoo Developers',
       author_email='devall@balihoo.com',
