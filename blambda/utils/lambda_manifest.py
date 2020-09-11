2c394eb9cab5f69f7ab0a2b0441680b7071d8a17
Devesh Doshi <deveshd@cybage.com>
2020-09-11 16:19:10 +0530

    add short_name in if condition

--- blambda/utils/lambda_manifest.py
+++ blambda/utils/lambda_manifest.py
@@ -146,7 +146,7 @@ class LambdaManifest(object):
 
                 for src in src_paths:
                     src = (self.basedir / src).resolve()
-                    dst = dest_dir / self.short_name / dst_pattern
+                    dst = dest_dir / dst_pattern
 
                     if '*' in dst.name:
                         dst = dst.parent / self.short_name / src.name
@@ -156,7 +156,7 @@ class LambdaManifest(object):
                 if dest_dir != self.basedir:
                     src = (self.basedir / source_spec).resolve()
                     if src.suffix == '.coffee' or src.suffix == '.js':
-                        yield src, dest_dir / source_spec
+                        yield src, dest_dir / self.short_name / source_spec
                     else:
                         yield src, dest_dir / source_spec
 
